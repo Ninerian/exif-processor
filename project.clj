2da522952ddbd00088de8d418b4d9947dc0b568a
Daniel Große <daniel.grosse@diginetmedia.de>
2017-01-17 10:50:18 +0100

    adds structured data, XMP parsing, XMP filtering, updates deps
    - adds function to get full XMP data
    - adds function which returns metadata as structured map
    - updates dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
-(defproject io.joshmiller/exif-processor "0.1.1"
+(defproject diginetmedia/exif-processor "0.2.0-SNAPSHOT"
   :description "A lightweight Clojure wrapper around the exif processing facility in Drew Oakes' metadata-extractor"
-  :url "http://github.com/joshuamiller/exif-processor"
+  :url "http://github.com/ninerian/exif-processor"
   :min-lein-version "2.1.0"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.drewnoakes/metadata-extractor "2.7.0-SNAPSHOT"]
-                 [clj-http "0.7.2"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.drewnoakes/metadata-extractor "2.9.1"]
+                 [clj-http "2.3.0"]]
+  :profiles {:dev {:resource-paths ["resources"]}}
+  :aliases {"release" ["deploy" "clojars"]})
+  )
--- src/exif_processor/core.clj
+++ src/exif_processor/core.clj
@@ -2,28 +2,86 @@
   (:use [clojure.string :only [join]])
   (:require [clj-http.client :as client])
   (:import [java.io BufferedInputStream FileInputStream]
-           [com.drew.imaging ImageMetadataReader]))
+           [com.drew.imaging ImageMetadataReader]
+           [com.drew.metadata.xmp XmpDirectory]))
 
 (def exif-directory-regex
   (re-pattern (str "(?i)(" (join "|"
-                                ["Exif" "JPEG" "JFIF"
-                                 "Agfa" "Canon" "Casio" "Epson"
-                                 "Fujifilm" "Kodak" "Kyocera"
-                                 "Leica" "Minolta" "Nikon" "Olympus"
-                                 "Panasonic" "Pentax" "Sanyo"
-                                 "Sigma/Foveon" "Sony"]) ")")))
+                                 ["Exif" "JPEG" "JFIF" "Photoshop"
+                                  "Agfa" "Canon" "Casio" "Epson"
+                                  "Fujifilm" "Kodak" "Kyocera"
+                                  "Leica" "Minolta" "Nikon" "Olympus"
+                                  "Panasonic" "Pentax" "Sanyo"
+                                  "Sigma/Foveon" "Sony"]) ")")))
 
-(defn- extract-from-tag
+(defn- get-metadata
+  "Get metadata for a File."
+  [^java.io.File file]
+  (-> file
+      (ImageMetadataReader/readMetadata)))
+
+
+(defn- fields->map
+  "Convert field entries to a clojure map."
   [tag]
   (into {} (map #(hash-map (.getTagName %) (.getDescription %)) tag)))
+
+
+(defn- filter-xmp
+	"Filters a seq of the XMP metadata by the given string"
+  [seq filter-str]
+  (let [pattern (re-pattern (str filter-str))]
+    (->> seq
+         (filter #(not (nil? (.getPath %))))
+         (filter #(re-find pattern (.toLowerCase (.getPath %)))))))
+
+(defn get-xmp-data
+	"Returns the XMP directory of the meta data as map. Could be filtered by a given string"
+  [metadata filter]
+  (let [xmp (.getFirstDirectoryOfType metadata (class (XmpDirectory.)))
+        xmp-meta 	(.getXMPMeta xmp)
+        data-seq (iterator-seq (.iterator xmp-meta))
+        filtered-data (filter-xmp data-seq filter)]
+    (into {}	(map #(hash-map (.getPath %)(.getValue %)) filtered-data))))
+
+(defn directories->map
+  "Generate a clojure map of directories/data from metadata."
+  [metadata]
+  (->> metadata
+       .getDirectories
+       (filter #(re-find exif-directory-regex (.getName %)))
+       (map (fn [directory]
+              [(.getName directory)
+               (-> directory
+                   .getTags
+                   fields->map)]))
+       (into {})))
+
+(defn exif->map
+  "Generate a clojure map of the metadata."
+  [metadata]
+  (->> metadata
+       .getDirectories
+       (filter #(re-find exif-directory-regex (.getName %)))
+       (map #(.getTags %))
+       (map fields->map)
+       (into {})))
+
 
 (defn exif-for-file
   "Takes an image file (as a java.io.InputStream or java.io.File) and extracts exif information into a map"
   [file]
-  (let [metadata (ImageMetadataReader/readMetadata file)
-        exif-directories (filter #(re-find exif-directory-regex (.getName %)) (.getDirectories metadata))
-        tags (map #(.getTags %) exif-directories)]
-    (into {} (map extract-from-tag tags))))
+  (let [metadata (get-metadata file)
+        data (exif->map metadata)
+        xmp (get-xmp-data metadata "gpano")]
+    (merge data xmp)))
+
+(defn exif-for-file-struct
+  [file]
+  (let [metadata (get-metadata file)
+        data (directories->map metadata)
+        xmp (get-xmp-data metadata "gpano")]
+    (merge data {"XMP" xmp})))
 
 (defn exif-for-filename
   "Loads a file from a give filename and extracts exif information into a map"
